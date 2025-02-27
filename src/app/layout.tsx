6db7c9ec57047f34b32516b8662b1d20323e374f
Data Salaryman <datasalaryman@protonmail.com>
2025-02-27 17:05:58 +0800

    fix: silence everything but frontend page for now

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import "@/styles/globals.css";
 import { GeistSans } from "geist/font/sans";
 import { type Metadata } from "next";
 
-import { TRPCReactProvider } from "@/trpc/react";
+// import { TRPCReactProvider } from "@/trpc/react";
 
 export const metadata: Metadata = {
   title: "Create T3 App",
@@ -17,7 +17,9 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${GeistSans.variable}`}>
       <body>
-        <TRPCReactProvider>{children}</TRPCReactProvider>
+        {/* <TRPCReactProvider> */}
+          {children}
+        {/* </TRPCReactProvider> */}
       </body>
     </html>
   );
