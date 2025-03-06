08e1377d11e910d84e11892a3e3de0a4dab96d81
Data Salaryman <datasalaryman@protonmail.com>
2025-03-06 16:49:51 +0800

    fix: only accept phantom wallets

--- src/app/_contexts/Wallet.tsx
+++ src/app/_contexts/Wallet.tsx
@@ -1,10 +1,10 @@
-"use client"
+"use client";
 
 import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
 import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
 import { clusterApiUrl } from '@solana/web3.js';
 import { useMemo } from 'react';
-import { UnsafeBurnerWalletAdapter } from '@solana/wallet-adapter-wallets';
+import { PhantomWalletAdapter } from '@solana/wallet-adapter-wallets';
 import {
   WalletModalProvider
 } from '@solana/wallet-adapter-react-ui';
@@ -18,8 +18,7 @@ export function WalletContext({
   const endpoint = useMemo(() => clusterApiUrl(network), [network]);
   const wallets = useMemo(
     () => [
-      // manually add any legacy wallet adapters here
-      new UnsafeBurnerWalletAdapter(),
+      new PhantomWalletAdapter(),
     ],
     [network],
   );
@@ -27,9 +26,9 @@ export function WalletContext({
   return (
     <ConnectionProvider endpoint={endpoint}>
       <WalletProvider wallets={wallets} autoConnect>
-        <WalletModalProvider>
+        {/* <WalletModalProvider> */}
           {children}
-        </WalletModalProvider>
+        {/* </WalletModalProvider> */}
       </WalletProvider>
     </ConnectionProvider>
   );
