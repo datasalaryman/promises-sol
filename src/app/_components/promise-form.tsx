d7bc03d5d244af33aedf6724c112f5c57c23cbfe
Data Salaryman <datasalaryman@protonmail.com>
2025-03-06 11:50:28 +0800

    fix: link date and time input to utc epoch time

--- src/app/_components/promise-form.tsx
+++ src/app/_components/promise-form.tsx
@@ -10,24 +10,73 @@ import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group"
 import { Textarea } from "@/components/ui/textarea"
 import { Calendar } from "@/components/ui/calendar"
 import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover"
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select"
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue, SelectGroup } from "@/components/ui/select"
 import { CalendarIcon } from "lucide-react"
-import { format } from "date-fns"
 import { cn } from "@/lib/utils"
 import { WalletDisconnectButton, WalletMultiButton } from "@solana/wallet-adapter-react-ui"
 import { useConnection, useWallet } from "@solana/wallet-adapter-react"
+import { DateTime } from "luxon"; 
 // Default styles that can be overridden by your app
 import '@solana/wallet-adapter-react-ui/styles.css';
 
 export const PromiseForm = () =>  {
+
+  const renderDate = DateTime.now().setZone("utc");
+
   const { connection } = useConnection();
   const [promiseContent, setPromiseContent] = useState("")
   const [promiseLamports, setPromiseLamports] = useState(10000000)
   const { publicKey } = useWallet();
-  const [date, setDate] = useState<Date>()
-  const [hour, setHour] = useState("12")
-  const [minute, setMinute] = useState("00")
-  const [period, setPeriod] = useState("PM")
+  const [epoch, setEpoch] = useState<number>(Math.floor(renderDate.toMillis() / (1000 * 60)) * 60);
+
+  // console.log(epoch)
+
+  const epochToDateOnly = (epochSeconds:number) : DateTime => {
+    const currentDateMills = Math.floor(epochSeconds / (60 * 60 * 24)) * 24 * 60 * 60 * 1000;
+
+    return DateTime.fromMillis(currentDateMills);
+  }
+
+  const epochToHourOnly = (epochSeconds:number) : number => {
+
+    const hmsEpochSeconds = epochSeconds - (epochToDateOnly(epochSeconds).toMillis() / 1000);
+
+    return Math.floor(hmsEpochSeconds / (60 * 60))
+
+  }
+
+  const epochToMinuteOnly = (epochSeconds:number) : number => {
+
+    const msEpochSeconds = epochSeconds - (epochToDateOnly(epochSeconds).toMillis() / 1000) - (epochToHourOnly(epochSeconds) * 60 * 60);
+
+    return Math.floor((msEpochSeconds) / 60)
+  }
+
+  const setEpochDate = (day: Date | undefined) => {
+
+    const hmsEpochSeconds = epoch - (epochToDateOnly(epoch).toMillis() / 1000);
+    const newDateEpochSeconds = Math.floor(DateTime.fromJSDate(day ?? renderDate.toJSDate()).setZone("utc", { keepLocalTime: true }).toMillis() / 1000)
+
+    setEpoch(newDateEpochSeconds + hmsEpochSeconds)
+
+  }
+
+  const setEpochHour = (hour: string) => {
+
+    const oldEpochHourSeconds = epochToHourOnly(epoch) * 60 * 60;
+    const newEpochHourSeconds = parseInt(hour) * 60 * 60;
+
+    setEpoch(epoch - oldEpochHourSeconds + newEpochHourSeconds)
+  }
+
+  const setEpochMinute = (minute: string) => {
+
+    const oldEpochMinuteSeconds = epochToMinuteOnly(epoch) * 60;
+    const newEpochMinuteSeconds = parseInt(minute) * 60;
+
+    setEpoch(epoch - oldEpochMinuteSeconds + newEpochMinuteSeconds)
+
+  }
 
   const handlePromiseChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     if (e.target.value.length <= 255) {
@@ -39,8 +88,7 @@ export const PromiseForm = () =>  {
     e.preventDefault()
     console.log({
       content: promiseContent,
-      date: date,
-      time: `${hour}:${minute} ${period}`,
+      epoch: epoch, 
       promiseLamports,
       wallet: publicKey?.toString()
     })
@@ -78,14 +126,14 @@ export const PromiseForm = () =>  {
                     <PopoverTrigger asChild>
                       <Button
                         variant="outline"
-                        className={cn("w-full justify-start text-left font-normal", !date && "text-muted-foreground")}
+                        className={cn("w-full justify-start text-left font-normal", !epochToDateOnly(epoch).toJSDate() && "text-muted-foreground")}
                       >
                         <CalendarIcon className="mr-2 h-4 w-4" />
-                        {date ? format(date, "PPP") : "Pick a date"}
+                        {epoch ? epochToDateOnly(epoch).toISODate() : "Pick a date"}
                       </Button>
                     </PopoverTrigger>
                     <PopoverContent className="w-auto p-0">
-                      <Calendar mode="single" selected={date} onSelect={setDate} initialFocus />
+                      <Calendar mode="single" selected={epochToDateOnly(epoch).toJSDate()} onSelect={setEpochDate} initialFocus />
                     </PopoverContent>
                   </Popover>
                 </div>
@@ -93,45 +141,39 @@ export const PromiseForm = () =>  {
                 <div className="space-y-2">
                   <Label>Time</Label>
                   <div className="flex gap-2">
-                    <Select value={hour} onValueChange={setHour}>
+                    <Select defaultValue={epochToHourOnly(epoch).toString()} onValueChange={setEpochHour}>
                       <SelectTrigger>
-                        <SelectValue placeholder="Hour" />
+                        <SelectValue placeholder="Hour"/>
                       </SelectTrigger>
                       <SelectContent>
-                        {Array.from({ length: 12 }, (_, i) => (i + 1).toString().padStart(2, "0")).map((h) => (
-                          <SelectItem key={h} value={h}>
-                            {h}
-                          </SelectItem>
-                        ))}
+                        <SelectGroup>
+                          {Array.from(Array(24).keys()).map((h) => (
+                            <SelectItem key={h} value={h.toString()}>
+                              {h.toString().padStart(2, "0")}
+                            </SelectItem>
+                          ))}
+                        </SelectGroup>
                       </SelectContent>
                     </Select>
 
-                    <Select value={minute} onValueChange={setMinute}>
+                    <Select defaultValue={"0"} onValueChange={setEpochMinute}>
                       <SelectTrigger>
                         <SelectValue placeholder="Min" />
                       </SelectTrigger>
                       <SelectContent>
-                        {Array.from({ length: 12 }, (_, i) => (i * 5).toString().padStart(2, "0")).map((m) => (
-                          <SelectItem key={m} value={m}>
-                            {m}
-                          </SelectItem>
-                        ))}
-                      </SelectContent>
-                    </Select>
-
-                    <Select value={period} onValueChange={setPeriod}>
-                      <SelectTrigger>
-                        <SelectValue placeholder="AM/PM" />
-                      </SelectTrigger>
-                      <SelectContent>
-                        <SelectItem value="AM">AM</SelectItem>
-                        <SelectItem value="PM">PM</SelectItem>
+                        <SelectGroup>
+                          {Array.from({length:12}, (_, i) => (i) * 5).map((m) => (
+                            <SelectItem key={m} value={m.toString()}>
+                              {m.toString().padStart(2, "0")}
+                            </SelectItem>
+                          ))}
+                        </SelectGroup>
                       </SelectContent>
                     </Select>
                   </div>
                 </div>
               </div>
-              <p className="text-sm text-muted-foreground">Note: Date and time will be recorded in UTC timezone</p>
+              <p className="text-sm text-muted-foreground">Note: Date and time in 24H format will be recorded in UTC timezone</p>
             </div>
 
             <div className="space-y-2">
