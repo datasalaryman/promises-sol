0907841deda8cb0289cb6bafe084043ab673cc41
Data Salaryman <datasalaryman@protonmail.com>
2025-03-16 10:59:53 +0800

    fix: add promise skeleton

--- program/programs/promisesprimitive/src/lib.rs
+++ program/programs/promisesprimitive/src/lib.rs
@@ -2,15 +2,143 @@ use anchor_lang::prelude::*;
 
 declare_id!("8sL8SYGgy7JPbzQzA2MXtZzuHvgG4j84CeuV9Rxd42jb");
 
+const AUTHOR: Pubkey = pubkey!("fTcVudr5vjBanSe9eYuX9HS3DuzjWKwavYBMbhLn2SJ");
+
 #[program]
 pub mod promisesprimitive {
+    use anchor_lang::solana_program::instruction::Instruction;
+
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+    pub fn make_self_promise(
+        ctx: Context<MakeSelfPromise>, 
+        text: Vec<u8>,
+        deadline_secs: u64, 
+        size: u64, 
+    ) -> Result<()> {
+        ctx.accounts.promise_account.bump = ctx.bumps.promise_account;
+        ctx.accounts.promise_account.size = size; 
+        ctx.accounts.promise_account.unix_seconds = deadline_secs; 
+        // let hold_promise: Instruction = anchor_lang::solana_program::system_instruction::transfer(
+        //     &ctx.accounts.signer.key(),
+        //     &ctx.accounts.promise_account.key(),
+        //     size,
+        // );
+        // let _ = anchor_lang::solana_program::program::invoke(
+        //     &hold_promise,
+        //     &[
+        //         ctx.accounts.signer.to_account_info(),
+        //         ctx.accounts.promise_account.to_account_info(),
+        //     ],
+        // );
+        msg!("Making a promise with {:?}", ctx.program_id);
         Ok(())
     }
+
+    pub fn fulfill_self_promise(
+        ctx: Context<FulFillSelfPromise>, 
+        text: Vec<u8>,
+        deadline_secs: u64, 
+        size: u64, 
+    ) -> Result<()> {
+        // let release_promise = anchor_lang::solana_program::system_instruction::transfer(
+        //     &ctx.accounts.promise_account.key(),
+        //     &ctx.accounts.signer.key(),
+        //     size,
+        // );
+        // let _ = anchor_lang::solana_program::program::invoke(
+        //     &release_promise,
+        //     &[
+        //         ctx.accounts.promise_account.to_account_info(),
+        //         ctx.accounts.signer.to_account_info(),
+        //     ],
+        // );
+        msg!("Fulfilling a promise with {:?}", ctx.program_id);
+        Ok(())
+    }
+
+    pub fn break_self_promise(
+        ctx: Context<BreakSelfPromise>, 
+        creator: Pubkey, 
+        text: Vec<u8>,
+        deadline_secs: u64, 
+        size: u64, 
+    ) -> Result<()> {
+        // let release_promise = anchor_lang::solana_program::system_instruction::transfer(
+        //     &ctx.accounts.promise_account.key(),
+        //     &ctx.accounts.signer.key(),
+        //     size,
+        // );
+        // let _ = anchor_lang::solana_program::program::invoke(
+        //     &release_promise,
+        //     &[
+        //         ctx.accounts.promise_account.to_account_info(),
+        //         ctx.accounts.signer.to_account_info(),
+        //     ],
+        // );
+        msg!("Breaking a promise with {:?}", ctx.program_id);
+        Ok(())
+    }
+
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+#[instruction(
+    text: Vec<u8>,
+    deadline_secs: u64, 
+    size: u64, 
+)]
+pub struct MakeSelfPromise<'info> {
+    #[account(mut)]
+    pub signer: Signer<'info>,
+    #[account(
+        init, 
+        payer = signer, 
+        space = 8 + 8 + 1, 
+        seeds = [b"selfpromise", signer.key().as_ref(), text.as_ref(), &deadline_secs.to_le_bytes().to_vec(), &size.to_le_bytes().to_vec()],
+        bump
+    )]
+    pub promise_account: Account<'info, SelfPromise>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+#[instruction(
+    text: Vec<u8>,
+    deadline_secs: u64, 
+    size: u64, 
+)]
+pub struct FulFillSelfPromise<'info> {
+    #[account(mut)]
+    pub signer: Signer<'info>,
+    #[account(
+        seeds = [b"selfpromise", signer.key().as_ref(), text.as_ref(), &deadline_secs.to_le_bytes().to_vec(), &size.to_le_bytes().to_vec()],
+        bump = promise_account.bump
+    )]
+    pub promise_account: Account<'info, SelfPromise>,
+}
+
+#[derive(Accounts)]
+#[instruction(
+    creator: Pubkey, 
+    text: Vec<u8>,
+    deadline_secs: u64, 
+    size: u64, 
+)]
+pub struct BreakSelfPromise<'info> {
+    #[account(address = AUTHOR)]
+    pub signer: Signer<'info>,
+    #[account(
+        seeds = [b"selfpromise", creator.key().as_ref(), text.as_ref(), &deadline_secs.to_le_bytes().to_vec(), &size.to_le_bytes().to_vec()],
+        bump = promise_account.bump,
+    )]
+    pub promise_account: Account<'info, SelfPromise>,
+}
+
+#[account]
+pub struct SelfPromise {
+    data: u64,
+    unix_seconds: u64, 
+    size: u64, 
+    bump: u8
+}
--- program/tests/promisesprimitive.ts
+++ program/tests/promisesprimitive.ts
@@ -1,16 +1,29 @@
 import * as anchor from "@coral-xyz/anchor";
-import { Program } from "@coral-xyz/anchor";
+import { BN, Program } from "@coral-xyz/anchor";
 import { Promisesprimitive } from "../target/types/promisesprimitive";
+import { assert } from "console";
+import { Keypair, SystemProgram } from "@solana/web3.js";
 
 describe("promisesprimitive", () => {
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.AnchorProvider.env());
+  const newAccountKp = new Keypair();
 
   const program = anchor.workspace.Promisesprimitive as Program<Promisesprimitive>;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
-  });
+
+  // it("able to make promise", async () => {
+  //   const text = [126, 234, 132, 45] as Array<number>
+  //   const deadlineSecs = new BN(1742351473)
+  //   const size = new BN(50000000)
+
+  //   await program
+  //     .methods
+  //     .makeSelfPromise(Buffer.from(text), deadlineSecs, size)
+  //     .accounts({
+  //       signer: newAccountKp.publicKey,
+  //     })
+  //     .signers([newAccountKp])?.rpc() ?? undefined;
+  // });
+
 });
