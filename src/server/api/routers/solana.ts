a82df45758d3e005f0fba925376ba25cf9b8ace7
Data Salaryman <datasalaryman@protonmail.com>
2025-03-27 15:17:56 +0800

    feat: add sol tx generation trpc routers

---
+++ src/server/api/routers/solana.ts
@@ -0,0 +1,82 @@
+import { z } from "zod";
+
+import { createTRPCRouter, publicProcedure } from "@/server/api/trpc";
+import { AnchorProvider, getProvider, Program, setProvider, web3, workspace } from "@coral-xyz/anchor";
+import { Promisesprimitive } from "target/types/promisesprimitive";
+import { PublicKey } from "@solana/web3.js";
+import { getSHA256Hash } from "boring-webcrypto-sha256"
+import { BN } from "bn.js";
+
+// setProvider(AnchorProvider.env());
+// const provider = getProvider()
+
+const program = workspace.Promisesprimitive as Program<Promisesprimitive>;
+
+export const solanaRouter = createTRPCRouter({
+  makePromiseGenerate: publicProcedure
+    .input(z.object({
+      signer: z.instanceof(PublicKey), 
+      text: z.string().length(255), 
+      deadline: z.number(), 
+      size: z.number()
+    }))
+    .output(z.instanceof(web3.TransactionInstruction))
+    .query( async ({ input }) => {
+
+      const textArray = [ ...Buffer.from(await getSHA256Hash(input.text), 'utf8') ].slice(0, 8)
+
+      const makeIx = await program
+        .methods
+        .makeSelfPromise(textArray, new BN(input.deadline), new BN(input.size))
+        .accounts({
+          signer: input.signer,
+        })
+        .instruction();
+      
+      return makeIx
+    }), 
+  fulfillPromiseGenerate: publicProcedure
+    .input(z.object({
+      signer: z.instanceof(PublicKey), 
+      text: z.string().length(255), 
+      deadline: z.number(), 
+      size: z.number()
+    }))
+    .output(z.instanceof(web3.TransactionInstruction))
+    .query( async ({ input }) => {
+
+      const textArray = [ ...Buffer.from(await getSHA256Hash(input.text), 'utf8') ].slice(0, 8)
+
+      const fulfillIx = await program
+        .methods
+        .fulfillSelfPromise(textArray, new BN(input.deadline), new BN(input.size))
+        .accounts({
+          signer: input.signer,
+        })
+        .instruction();
+      
+      return fulfillIx
+    }),
+  breakPromiseGenerate: publicProcedure
+    .input(z.object({
+      creator: z.instanceof(PublicKey), 
+      text: z.string().length(255), 
+      deadline: z.number(), 
+      size: z.number()
+    }))
+    .output(z.instanceof(web3.TransactionInstruction))
+    .query( async ({ input }) => {
+
+      const textArray = [ ...Buffer.from(await getSHA256Hash(input.text), 'utf8') ].slice(0, 8)
+
+      const breakIx = await program
+        .methods
+        .breakSelfPromise(textArray, new BN(input.deadline), new BN(input.size))
+        .accounts({
+          creator: input.creator,
+        })
+        .instruction();
+      
+      return breakIx
+    })
+});
