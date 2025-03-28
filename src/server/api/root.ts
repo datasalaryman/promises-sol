3be87ff990fe66db284785e471391099a0b1060e
Data Salaryman <datasalaryman@protonmail.com>
2025-03-28 10:26:36 +0800

    fix: add solana router to trpc

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,6 @@
 import { promiseRouter } from "@/server/api/routers/promise";
 import { createCallerFactory, createTRPCRouter } from "@/server/api/trpc";
+import { solanaRouter } from "@/server/api/routers/solana";
 
 /**
  * This is the primary router for your server.
@@ -8,6 +9,7 @@ import { createCallerFactory, createTRPCRouter } from "@/server/api/trpc";
  */
 export const appRouter = createTRPCRouter({
   promise: promiseRouter,
+  solana: solanaRouter,
 });
 
 // export type definition of API
