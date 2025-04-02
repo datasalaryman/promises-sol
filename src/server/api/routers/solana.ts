10a26f626e1b804e3978a93c181e4df1648aace5
Data Salaryman <datasalaryman@protonmail.com>
2025-04-02 13:07:47 +0800

    fix: use node crypto library for hasing input text

--- src/server/api/routers/solana.ts
+++ src/server/api/routers/solana.ts
@@ -4,23 +4,35 @@ import { createTRPCRouter, publicProcedure } from "@/server/api/trpc";
 import * as anchor from "@coral-xyz/anchor";
 import idl from "@/idl/promisesprimitive.json"
 import { PublicKey } from "@solana/web3.js";
-import { getSHA256Hash } from "boring-webcrypto-sha256"
+import { createHash } from "crypto";
 import { BN } from "bn.js";
+import { Promisesprimitive } from "@/types/promisesprimitive";
 
-const program = new anchor.Program(idl, anchor.AnchorProvider.local());;
+const program = new anchor.Program<Promisesprimitive>(
+  idl,
+  anchor.AnchorProvider.local()
+);;
 
 export const solanaRouter = createTRPCRouter({
   makePromiseGenerate: publicProcedure
     .input(z.object({
-      signer: z.string().nullable(), 
-      text: z.string().max(255), 
-      deadline: z.number(), 
+      signer: z.string().nullish(),
+      text: z.string().max(255),
+      deadline: z.number(),
       size: z.number()
     }))
-    .output(z.instanceof(anchor.web3.TransactionInstruction))
+    .output(z.instanceof(anchor.web3.TransactionInstruction).nullish())
     .query( async ({ input }) => {
 
-      const textArray = [ ...Buffer.from(await getSHA256Hash(input.text), 'utf8') ].slice(0, 8)
+      if (!input.signer) {
+        return null
+      }
+
+      const textArray = Array.from(
+        Uint8Array.from(
+          createHash("sha256").update(input.text).digest(),
+        ).subarray(0, 8),
+      );
 
       const makeIx = await program
         .methods
@@ -29,20 +41,24 @@ export const solanaRouter = createTRPCRouter({
           signer: new PublicKey(input?.signer),
         })
         .instruction();
-      
-      return makeIx
-    }), 
+
+      return input.signer ? makeIx : null
+    }),
   fulfillPromiseGenerate: publicProcedure
     .input(z.object({
-      signer: z.instanceof(PublicKey), 
-      text: z.string().length(255), 
-      deadline: z.number(), 
+      signer: z.instanceof(PublicKey),
+      text: z.string().length(255),
+      deadline: z.number(),
       size: z.number()
     }))
     .output(z.instanceof(anchor.web3.TransactionInstruction))
     .query( async ({ input }) => {
 
-      const textArray = [ ...Buffer.from(await getSHA256Hash(input.text), 'utf8') ].slice(0, 8)
+      const textArray = Array.from(
+        Uint8Array.from(
+          createHash("sha256").update(input.text).digest(),
+        ).subarray(0, 8),
+      );
 
       const fulfillIx = await program
         .methods
@@ -51,20 +67,24 @@ export const solanaRouter = createTRPCRouter({
           signer: input.signer,
         })
         .instruction();
-      
+
       return fulfillIx
     }),
   breakPromiseGenerate: publicProcedure
     .input(z.object({
-      creator: z.instanceof(PublicKey), 
-      text: z.string().length(255), 
-      deadline: z.number(), 
+      creator: z.instanceof(PublicKey),
+      text: z.string().length(255),
+      deadline: z.number(),
       size: z.number()
     }))
     .output(z.instanceof(anchor.web3.TransactionInstruction))
     .query( async ({ input }) => {
 
-      const textArray = [ ...Buffer.from(await getSHA256Hash(input.text), 'utf8') ].slice(0, 8)
+      const textArray = Array.from(
+        Uint8Array.from(
+          createHash("sha256").update(input.text).digest(),
+        ).subarray(0, 8),
+      );
 
       const breakIx = await program
         .methods
@@ -73,7 +93,7 @@ export const solanaRouter = createTRPCRouter({
           creator: input.creator,
         })
         .instruction();
-      
+
       return breakIx
     })
 });
