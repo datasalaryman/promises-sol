905f5e800b67805094b70a607ed22a501775d06a
Data Salaryman <datasalaryman@protonmail.com>
2025-04-14 10:11:28 +0800

    feat: add how it works page

---
+++ src/app/how-it-works/page.tsx
@@ -0,0 +1,10 @@
+import Content from "@/app/how-it-works/content.mdx";
+import { MdxLayout } from "../layout";
+
+export default function HowItWorksPage() {
+  return(
+    <MdxLayout>
+      <Content />
+    </MdxLayout>
+  );
+}
