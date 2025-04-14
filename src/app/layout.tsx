54cfcf0bd3df21216615ddf31f34714f047b702f
Data Salaryman <datasalaryman@protonmail.com>
2025-04-14 10:11:12 +0800

    fix: add tailwind typography and custom styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,4 +25,17 @@ export default function RootLayout({
       </body>
     </html>
   );
+};
+
+export function MdxLayout({ children }: { children: React.ReactNode }) {
+  // Create any shared layout or styles here
+  return (
+    <div className="flex flex-col items-center">
+      <div className="px-16 sm:px-40">
+        <article className="prose prose-slate">
+          {children}
+        </article>
+      </div>
+    </div>
+  )
 }
