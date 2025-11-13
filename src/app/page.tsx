8a2ac510af80a25faaa0be58e4673d7019f367d7
Data Salaryman <datasalaryman@protonmail.com>
2025-11-13 12:44:30 +0800

    fix: move app contexts to layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,4 @@
 import { PromiseForm } from "@/app/_components/promise-form";
-import { WalletContext } from "@/app/_contexts/Wallet";
-import { TRPCReactProvider } from "@/trpc/react";
 
 export default async function Home() {
   return (
@@ -9,11 +7,7 @@ export default async function Home() {
         <h1 className="text-center text-xl font-bold text-gray-800 sm:text-2xl">
           Make and keep your own promises, with SOL!
         </h1>
-        <WalletContext>
-          <TRPCReactProvider>
-            <PromiseForm />
-          </TRPCReactProvider>
-        </WalletContext>
+        <PromiseForm />
       </div>
     </main>
   );
