da89902ec7751fc6c1ea8d0b8c3e3ba4f1c0acf1
Data Salaryman <datasalaryman@protonmail.com>
2025-11-13 13:06:16 +0800

    fix: ensure wallet goes to top of page in smaller screen sizes

--- src/app/_components/Navbar.tsx
+++ src/app/_components/Navbar.tsx
@@ -23,7 +23,12 @@ export function Navbar() {
 
   return (
     <header className="w-full px-6 py-3">
-      <nav className="min-w-full flex justify-between">
+      <nav className="min-w-full flex flex-col gap-4 sm:flex-row-reverse sm:justify-between">
+        {publicKey ? (
+          <WalletDisconnectButtonDynamic />
+        ) : (
+          <WalletMultiButtonDynamic />
+        )}
         <div className="flex max-w-7xl justify-center items-center space-x-8">
           <Link
             href="/"
@@ -44,11 +49,6 @@ export function Navbar() {
             How it works
           </Link>
         </div>
-          {publicKey ? (
-            <WalletDisconnectButtonDynamic />
-          ) : (
-            <WalletMultiButtonDynamic />
-          )}
       </nav>
     </header>
   );
