ce5a01553af6d2baa52ede41ee5b345770771649
Data Salaryman <datasalaryman@protonmail.com>
2025-11-27 19:45:39 +0800

    fix: cleanup landing page

--- src/app/_components/Navbar.tsx
+++ src/app/_components/Navbar.tsx
@@ -11,7 +11,7 @@ export function Navbar() {
     <header className="w-full px-6 py-3">
       <nav className="min-w-full flex flex-col gap-4 sm:flex-row-reverse sm:justify-between">
         <WalletUiDropdown />
-        <div className="flex max-w-7xl justify-center items-center space-x-8">
+        <div className="flex flex-col sm:flex-row max-w-7xl justify-center items-center space-x-8 gap-6">
           <Link href="/" className="flex items-center space-x-2">
             <Image
               src="/promisekeepr.svg"
@@ -22,24 +22,26 @@ export function Navbar() {
             />
             <span className="text-xl font-serif font-bold">Promisekeepr</span>
           </Link>
-          <Link
-            href="/"
-            className="text-muted-foreground transition-colors hover:text-foreground inline"
-          >
-            Create Promise
-          </Link>
-          <Link
-            href="/dash"
-            className="text-muted-foreground transition-colors hover:text-foreground"
-          >
-            View Promises
-          </Link>
-          <Link
-            href="/how-it-works"
-            className="text-muted-foreground transition-colors hover:text-foreground"
-          >
-            How it works
-          </Link>
+          <div className="flex flex-row space-x-4">
+            <Link
+              href="/"
+              className="text-muted-foreground transition-colors hover:text-foreground inline"
+            >
+              Create Promise
+            </Link>
+            <Link
+              href="/dash"
+              className="text-muted-foreground transition-colors hover:text-foreground"
+            >
+              View Promises
+            </Link>
+            <Link
+              href="/how-it-works"
+              className="text-muted-foreground transition-colors hover:text-foreground"
+            >
+              How it works
+            </Link>
+          </div>
         </div>
       </nav>
     </header>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,18 +8,13 @@ export default function Home() {
   const { account, cluster } = useWalletUi();
   return (
     <main className="flex h-fit items-center justify-center">
-      <div className="flex flex-col items-center gap-6 pt-8">
-        <h1 className="text-center text-xl font-bold text-foreground sm:text-2xl">
-          Make and keep your own promises, with SOL!
-        </h1>
-        {account ? 
-          <div className="flex flex-row gap-8 px-4">
-            <PromiseForm account={account} cluster={cluster} />
-            <RequestForm account={account} disabled={true}/>
-          </div> : 
-          <p>Please connect your wallet</p>
-        }
-      </div>
+      {account ? 
+        <div className="flex flex-col sm:flex-row gap-2 sm:gap-6 px-4">
+          <PromiseForm account={account} cluster={cluster} />
+          <RequestForm account={account} disabled={true}/>
+        </div> : 
+        <p>Please connect your wallet</p>
+      }
     </main>
   );
 }
